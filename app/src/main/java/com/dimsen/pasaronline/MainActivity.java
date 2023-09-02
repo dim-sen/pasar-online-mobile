1025b5adab3593c39b1a847c0480d3b82daa5428
dim-sen <dimas.10119285@mahasiswa.unikom.ac.id>
2023-09-02 23:33:03 +0700

    bottom nav

--- app/src/main/java/com/dimsen/pasaronline/MainActivity.java
+++ app/src/main/java/com/dimsen/pasaronline/MainActivity.java
@@ -1,8 +1,13 @@
 package com.dimsen.pasaronline;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavController navController = Navigation.findNavController(this, R.id.homeHostFragment);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNav);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
     }
 }
